0fc48b3efd625c51b74e503a772afb0ce3a4689b
Ben <benhodeda@gmail.com>
2016-08-20 16:19:10 +0300

    add paypal

--- api/paypal/paypal.controller.js
+++ api/paypal/paypal.controller.js
@@ -1,73 +1,121 @@
 module.exports = PayPalController;
 
-var paypal = require('paypal-rest-sdk');
-var config = require('./config.json');
+// var paypal = require('paypal-rest-sdk');
+// var config = require('./config.json');
+var Paypal = require('paypal-adaptive');
+var Logs = require('../products/products.service');
+var logs = new Logs();
+
+var paypalSdk = new Paypal({
+    userId:    '489766946',
+    password:  'KTQNRF6DZNUZN7DR',
+    signature: 'AFcWxV21C7fd0v3bYYYRCpSSRl31AgmkuMGQzQRxJ1-F.aPTMkW7rTnk',
+    sandbox:   true //defaults to false
+});
 
 function PayPalController() {
     var self = this;
 
-    self.init = init;
-    self.create = create;
-    self.execute = execute;
+    self.preapproval = preapproval;
 
-    function init(){
-        paypal.configure(config.api);
-    }
-
-    function create (method, currency, amount, description, card) {
-        var payment = {
-            "intent": "sale",
-            "payer": {
+    function preapproval(){
+        var payload = {
+            requestEnvelope: {
+                errorLanguage:  'en_US'
             },
-            "transactions": [{
-                "amount": {
-                    "currency": currency,
-                    "total": amount
-                },
-                "description": description
-            }]
+            actionType:     'PAY',
+            currencyCode:   'USD',
+            feesPayer:      'EACHRECEIVER',
+            memo:           'Chained payment example',
+            cancelUrl:      'http://localhost:3000/?x=cancel',
+            returnUrl:      'http://localhost:3000/?x=success',
+            receiverList: {
+                receiver: [
+                    {
+                        email:  'eladdo@gmail.com',
+                        amount: '100.00',
+                        primary:'true'
+                    },
+                    {
+                        email:  'benhodeda@gmail.com',
+                        amount: '10.00',
+                        primary:'false'
+                    }
+                ]
+            }
         };
 
-        if (method === 'paypal') {
-            payment.payer.payment_method = 'paypal';
-            payment.redirect_urls = {
-                "return_url": "https://mta-shopapi.herokuapp.com/api/paypal/execute",
-                "cancel_url": "https://mta-shopapi.herokuapp.com/api/paypal/cancel"
-            };
-        } else if (method === 'credit_card') {
-            var funding_instruments = [
-                {
-                    "credit_card": {
-                        "type": card.type,
-                        "number": card.number,
-                        "expire_month": card.expire_month,
-                        "expire_year": card.expire_year,
-                        "first_name": card.first_name,
-                        "last_name": card.last_name
-                    }
-                }
-            ];
-            payment.payer.payment_method = 'credit_card';
-            payment.payer.funding_instruments = funding_instruments;
-        }
-
-        return paypal.payment.create(payment, function (error, payment) {
-            if (error) {
-                throw error;
+        paypalSdk.preapproval(payload, function (err, response) {
+            if (err) {
+                logs.index(err);
             } else {
-                return { 'payment': payment };
+                logs.index(response);
             }
         });
     }
 
-    function execute(paymentId, payerId) {
-        var details = { "payer_id": payerId };
-        return paypal.payment.execute(paymentId, details, function (error, payment) {
-            if (error) {
-                throw error;
-            } else {
-                return { 'payment': payment };
-            }
-        });
-    }
+    // self.init = init;
+    // self.create = create;
+    // self.execute = execute;
+
+    // function init(){
+    //     paypal.configure(config.api);
+    // }
+    //
+    // function create (method, currency, amount, description, card) {
+    //     var payment = {
+    //         "intent": "sale",
+    //         "payer": {
+    //         },
+    //         "transactions": [{
+    //             "amount": {
+    //                 "currency": currency,
+    //                 "total": amount
+    //             },
+    //             "description": description
+    //         }]
+    //     };
+    //
+    //     if (method === 'paypal') {
+    //         payment.payer.payment_method = 'paypal';
+    //         payment.redirect_urls = {
+    //             "return_url": "https://mta-shopapi.herokuapp.com/api/paypal/execute",
+    //             "cancel_url": "https://mta-shopapi.herokuapp.com/api/paypal/cancel"
+    //         };
+    //     } else if (method === 'credit_card') {
+    //         var funding_instruments = [
+    //             {
+    //                 "credit_card": {
+    //                     "type": card.type,
+    //                     "number": card.number,
+    //                     "expire_month": card.expire_month,
+    //                     "expire_year": card.expire_year,
+    //                     "first_name": card.first_name,
+    //                     "last_name": card.last_name
+    //                 }
+    //             }
+    //         ];
+    //         payment.payer.payment_method = 'credit_card';
+    //         payment.payer.funding_instruments = funding_instruments;
+    //     }
+    //
+    //     return paypal.payment.create(payment, function (error, payment) {
+    //         if (error) {
+    //             throw error;
+    //         } else {
+    //             return { 'payment': payment };
+    //         }
+    //     });
+    // }
+    //
+    // function execute(paymentId, payerId) {
+    //     var details = { "payer_id": payerId };
+    //     return paypal.payment.execute(paymentId, details, function (error, payment) {
+    //         if (error) {
+    //             throw error;
+    //         } else {
+    //             return { 'payment': payment };
+    //         }
+    //     });
+    // }
 }
--- api/paypal/paypal.routes.js
+++ api/paypal/paypal.routes.js
@@ -3,34 +3,40 @@ var controller = new PayPalController();
 var express = require('express');
 var router = express.Router();
 
-router.get('/create', function(req, res, next) {
-    var method = req.param('method');
-    var currency = req.param('currency');
-    var amount = req.param('amount');
-    var description = req.param('description');
-    var card = {
-        type: req.param('type').toLowerCase(),
-        number: req.param('number'),
-        expire_month: req.param('expire_month'),
-        expire_year: req.param('expire_year'),
-        first_name: req.param('first_name'),
-        last_name: req.param('last_name')
-    };
-    return controller.create(method,currency,amount,description,card).then(function(payment){
-        req.session.paymentId = payment.id;
-        return payment;
-    })
-});
-
-router.get('/execute', function (req,res,next){
-    var paymentId = req.session.paymentId;
-    var payerId = req.param('PayerID');
-    return controller.execute(paymentId, payerId);
+router.post('/', function (req, res, next) {
+    return controller.preapproval();
 });
 
 
-router.get('/cancel', function (req,res,next){
-    res.status()
-});
+//
+// router.get('/create', function(req, res, next) {
+//     var method = req.param('method');
+//     var currency = req.param('currency');
+//     var amount = req.param('amount');
+//     var description = req.param('description');
+//     var card = {
+//         type: req.param('type').toLowerCase(),
+//         number: req.param('number'),
+//         expire_month: req.param('expire_month'),
+//         expire_year: req.param('expire_year'),
+//         first_name: req.param('first_name'),
+//         last_name: req.param('last_name')
+//     };
+//     return controller.create(method,currency,amount,description,card).then(function(payment){
+//         req.session.paymentId = payment.id;
+//         return payment;
+//     })
+// });
+//
+// router.get('/execute', function (req,res,next){
+//     var paymentId = req.session.paymentId;
+//     var payerId = req.param('PayerID');
+//     return controller.execute(paymentId, payerId);
+// });
+//
+//
+// router.get('/cancel', function (req,res,next){
+//     res.status()
+// });
 
 module.exports = router;
