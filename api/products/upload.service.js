8c1774513d47b5eedc8ce0beee2557d1c79a854c
Ben <benhodeda@gmail.com>
2016-08-25 17:34:49 +0300

    upload images to mongo

--- api/products/upload.service.js
+++ api/products/upload.service.js
@@ -1,14 +1,7 @@
 var multer  =   require('multer');
 var uuid = require('uuid');
-var fs = require('fs');
 
-var dir = './api/products/uploads';
-
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-
-var storage = require('multer-gridfs-storage')({
+var storage = require('gridfs-storage-engine')({
     url: 'mongodb://heroku_t2515wmc:nn2b48909ctombbf7ubp279v4k@ds011248.mongolab.com:11248/heroku_t2515wmc',
     filename: function (req, file, callback) {
         var ext = file.originalname.split('.');
